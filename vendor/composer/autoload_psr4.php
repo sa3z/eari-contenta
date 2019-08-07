2b2858e68533321dcba6d5a926fa244e67e8431f
Shalini Anand <sanand@aaas.org>
2019-08-07 18:50:14 -0400

    adding back missing modules that were improperly uninstalled: graphql

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -67,6 +67,7 @@ return array(
     'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
     'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
+    'GraphQL\\' => array($vendorDir . '/webonyx/graphql-php/src'),
     'Goutte\\' => array($vendorDir . '/fabpot/goutte/Goutte'),
     'Egulias\\EmailValidator\\' => array($vendorDir . '/egulias/email-validator/EmailValidator'),
     'Drush\\' => array($vendorDir . '/drush/drush/src'),
